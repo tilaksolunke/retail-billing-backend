d73d156add71fef3294299d018c568c4a8b0b702
tilaksolunke <tilaksolunke2002@gmail.com>
2025-08-11 10:05:08 +0530

    Itegrate stripe payment gateway, Create APIs for Dashboard module and Store images locally

---
+++ src/main/java/in/tilaksolunke/billingsoftware/controller/DashboardController.java
@@ -0,0 +1,34 @@
+package in.tilaksolunke.billingsoftware.controller;
+
+import in.tilaksolunke.billingsoftware.io.DashboardResponse;
+import in.tilaksolunke.billingsoftware.io.OrderResponse;
+import in.tilaksolunke.billingsoftware.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/dashboard")
+@RequiredArgsConstructor
+public class DashboardController {
+
+    private final OrderService orderService;
+
+    @GetMapping
+    public DashboardResponse getDashboardData() {
+        LocalDate today = LocalDate.now();
+        Double todaySale = orderService.sumSalesByDate(today);
+        Long todayOrderCount = orderService.countByOrderDate(today);
+        List<OrderResponse> recentOrders = orderService.findRecentOrders();
+        return new DashboardResponse(
+                todaySale != null ? todaySale : 0.0,
+                todayOrderCount != null ? todayOrderCount : 0,
+                recentOrders
+        );
+    }
+}
+
